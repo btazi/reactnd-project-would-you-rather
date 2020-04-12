5aa7b7429b89232a593f2a710d5280715305d0b5
badr tazi <tasibadr@gmail.com>
2020-04-12 12:36:24 +0100

    add state, reducers, redux, redux-thunk, redux devtools

---
+++ src/reducers/questions.js
@@ -0,0 +1,6 @@
+export default function Questions(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/reducers/users.js
@@ -0,0 +1,6 @@
+export default function Users(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
